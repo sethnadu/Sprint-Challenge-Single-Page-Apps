08870e017875353b60f3755a77afc168466ae91d
Seth Nadu <seth.nadu@gmail.com>
2019-07-26 13:47:14 -0400

    tried ternary for buttons, attempted search from

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,15 +1,15 @@
 import React, { useEffect, useState } from 'react';
 import CharacterCard from "./CharacterCard.js";
-import SearchForm from "./SearchForm.js";
+// import SearchForm from "./SearchForm.js";
 
-import { Route } from "react-router-dom";
+// import { Route } from "react-router-dom";
 import axios from "axios";
 import {TabHeading, ButtonDiv, Buttons} from "./styles.js";
 
 export default function CharacterList() {
   const [character, setCharacter] = useState('');
   const [change, setChange] = useState("https://rickandmortyapi.com/api/character/?page=1")
-  const [form, setForm] = useState("")
+  // const [form, setForm] = useState("")
   
 
   useEffect(() => {
@@ -18,10 +18,12 @@ export default function CharacterList() {
       .get(`${change}`)
       .then(response => {
         setCharacter(response.data.results)
+        console.log(response.data.info)
         setChange(response.data.info)
-        setForm(Object.values(response.data.results.map(props => ({
-                name: props.name
-        }))));
+        
+        // setForm(Object.values(response.data.results.map(props => ({
+        //         name: props.name
+        // }))));
         
         
         
@@ -32,6 +34,8 @@ export default function CharacterList() {
       });
   }, [change]);
 
+                        // Search From Below
+
     // const onSearch = (name) => {
     //   const formCopy = [...form];
     
@@ -39,25 +43,35 @@ export default function CharacterList() {
     //   Object.assign(findPerson, name);
 
     // }
+                        // Search Form End
 
+  return (
 
-  return <section>
-         <TabHeading>Characters</TabHeading>
-        <ButtonDiv>
+   <section>
+
+      <TabHeading>Characters</TabHeading>
+      <ButtonDiv>
+              {/* {change.prev.IsNullOrEmpty("") ? 
+              <Buttons onClick ={() => setChange(change.next.length-1)}>Previous</Buttons>
+              : */}
         <Buttons onClick = {() => setChange(change.prev)}>Previous</Buttons>
-        {/* <Route path ="/character/:name" render = {props => {
-          return <SearchForm {...props} onSearch = {onSearch}  />
-        }} /> */}
-        
+            {/* } */}
+
+                      {/* Below is for SearchForm */}
+            {/* <Route path ="/character/:name" render = {props => {
+              return <SearchForm {...props} onSearch = {onSearch}  />
+            }} /> */}
         <Buttons onClick = {() => setChange(change.next)}>Next</Buttons>
-        </ButtonDiv>
-        <div className='character-list grid-view'>
+      </ButtonDiv>
+
+      <div className='character-list grid-view'>
         {Object.values(character).map(props => {
-        // console.log(props)
-        return (  <CharacterCard props = {props}  key = {props.id} />
-        )
+          // console.log(props)
+          return (  <CharacterCard props = {props}  key = {props.id} />
+          )
         })}
-        </div>
-    </section>
+      </div>
+  </section>
+    )
 
 }
